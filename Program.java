86f91c28c42b11885adffd231aa2bee627fcb317
Subhrojeet Dutta <106841956+geeksubhro@users.noreply.github.com>
2023-09-28 16:17:17 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,24 @@
+package com.anudip.Test;
+
+public class Program {
+	public static void main(String args[])
+	{
+//		Here are the Start and End
+	int x=25;
+	int y=51;
+//	Here the Sum of Even Numbers is saved
+	int sum=0;
+//	Loop For iterating over All the Numbers
+	for (int i=x;x<=51;x++)
+		{
+//		Checking Even Numbers
+			if (x%2==0)
+				sum+=i;
+		}
+//	Printing The Sum
+	System.out.println("The Sum of Even Numbers is "+sum);
+}
+	}
+//1. Write a program to find the sum 
+//of even numbers between 25 to 51.
+//
---
+++ Program2.java
@@ -0,0 +1,70 @@
+package com.anudip.Test;
+
+import java.util.Scanner;
+
+
+public class Program2 {
+	//here are the questions too choose from
+	static void call()
+	{
+		System.out.println("Choose From the Following:");
+		System.out.println("1. Addition");
+		System.out.println("2. Substraction");
+		System.out.println("3. Multiplication");
+		System.out.println("4. Division");
+		System.out.println("5. Change Numbers");
+		System.out.println("6. Exit");
+		System.out.println("Enter Your Choice :");
+	}
+
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Two Numbers :");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		call();
+		//this x will decide which case should be choosen
+		int x=sc.nextInt();
+		while(true) 
+		{
+			switch(x)
+			{
+			case 1:
+				//this where addition takes place
+				System.out.println("The Sum of Two Numbers is :"+(a+b));
+				break;
+			case 2:
+				//this where Substraction takes place
+				System.out.println("The Difference of Two Numbers is :"+(a-b));
+				break;
+			case 3:
+				//this where Multiplication takes place
+				System.out.println("The Multiplication of Two Numbers is :"+(a*b));
+				break;
+			case 4:
+				//this where Division takes place
+				System.out.println("The Division of Two Numbers is :"+(a/b));
+				break;
+			case 5:
+				//Here You can change the numbers
+				System.out.println("Enter Two Numbers :");
+				a=sc.nextInt();
+				b=sc.nextInt();
+				break;
+			case 6:
+				//this where the program takes a exit
+				sc.close();
+				System.exit(0);
+				default:
+					System.out.println("Wrong User Input");
+			}
+			call();
+			x=sc.nextInt();
+		}
+
+}
+}
+//2. Write a program to Menu driven program using 
+//switch statement ( Find addition, subtraction,
+//		multiplication and division of to integer numbers )
